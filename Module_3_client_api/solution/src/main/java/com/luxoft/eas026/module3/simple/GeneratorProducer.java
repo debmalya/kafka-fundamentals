28c980482b77bea303c2dd18471bc958d7637336
VOpolskiy <vadopolski@gmail.com>
2022-10-24 00:45:54 +0200

    Refactoring

--- Module_3_client_api/solution/src/main/java/com/luxoft/eas026/module3/simple/GeneratorProducer.java
+++ Module_3_client_api/solution/src/main/java/com/luxoft/eas026/module3/simple/GeneratorProducer.java
@@ -25,13 +25,7 @@ public class GeneratorProducer {
 
 	private static Producer<String, Integer> producer;
 
-	public static void throwException() throws InterruptedException {
-		throw new InterruptedException();
-	}
-
-
 	public static void main(String[] args) {
-
 		Properties props = new Properties();
 		props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
 		props.put(ProducerConfig.CLIENT_ID_CONFIG, CLIENT_ID);
@@ -41,45 +35,19 @@ public class GeneratorProducer {
 		producer = new KafkaProducer<>(props);
 
 		ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
-		executor.scheduleAtFixedRate(() -> {
-			try {
-				send(args[0]);
-			} catch (InterruptedException e) {
-				ProducerRecord<String, Integer> data1 = new ProducerRecord<>("error_topic", "key_error", 2);
-				try {
-
-					RecordMetadata meta = producer.send(data1).get();
-				} catch (InterruptedException ex) {
-					throw new RuntimeException(ex);
-				} catch (ExecutionException ex) {
-					throw new RuntimeException(ex);
-				}
-
-			} catch (ExecutionException e) {
-				throw new RuntimeException(e);
-			} finally {
-				producer.flush();
-			}
-		}, 0, 3, TimeUnit.SECONDS);
+		executor.scheduleAtFixedRate(() -> send(args[0]), 0, 3, TimeUnit.SECONDS); // no blocking
 
 		Runtime.getRuntime().addShutdownHook(new Thread(producer::close));
 	}
 
 	@SuppressWarnings({ "boxing", "unused" })
-	public static void send(String topic) throws InterruptedException, ExecutionException {
-		final int number = new Random().nextInt(3);
-
-		if (number == 2 ) {
-			throwException();
-		}
+	public static void send(String topic) {
+		final int number = new Random().nextInt(10);
 		ProducerRecord<String, Integer> data = new ProducerRecord<>(topic, "key" + number, number);
-
-//		ProducerRecord<String, Integer> data = new ProducerRecord<>(topic, "key_string_1_part", number);
 		try {
 			RecordMetadata meta = producer.send(data).get();
 			LOG.info("key = {}, value = {} => partition = {}, offset= {}", data.key(), data.value(), meta.partition(), meta.offset());
 		} catch (InterruptedException | ExecutionException e) {
-		} finally {
 			producer.flush();
 		}
 
---
+++ Module_3_client_api/solution/src/main/java/com/luxoft/eas026/module3/simple/OnlyLastMessagesConsumer.java
@@ -0,0 +1,67 @@
+package com.luxoft.eas026.module3.simple;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.PartitionInfo;
+import org.apache.kafka.common.serialization.IntegerDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.TopicPartition;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.*;
+
+public class OnlyLastMessagesConsumer {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ExampleConsumer.class);
+
+    private static final String BOOTSTRAP_SERVERS = ":9092";
+    private static final String CONSUMER_GROUP = "first";
+    private static final String OFFSET_RESET = "earliest";
+//	private static final String OFFSET_RESET = "latest"; // default
+
+
+    @SuppressWarnings("boxing")
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, CONSUMER_GROUP); // !!!!! fixed
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, OFFSET_RESET); // !!!!!
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, IntegerDeserializer.class.getName());
+        props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 15);
+
+
+        try (KafkaConsumer<String, Integer> consumer = new KafkaConsumer<>(props)) {
+            String topic = args[0];
+            List<PartitionInfo> partitions = consumer.partitionsFor(topic);
+            ArrayList<TopicPartition> topicPartitions = new ArrayList<>();
+
+            for (PartitionInfo part: partitions) {
+                topicPartitions.add(new TopicPartition(topic, part.partition()));
+            }
+
+            Map<TopicPartition, Long> endOffsets = consumer.endOffsets(topicPartitions);
+
+            consumer.assign(topicPartitions);
+
+            for (Map.Entry<TopicPartition, Long> entry : endOffsets.entrySet()) {
+                consumer.seek(entry.getKey(), (entry.getValue()));
+            }
+
+            ConsumerRecords<String, Integer> records = consumer.poll(Duration.ofSeconds(2));
+
+            // max time
+            for (ConsumerRecord<String, Integer> data : records) {
+                LOG.info("key = {}, value = {} => partition = {}, offset= {}", data.key(), data.value(), data.partition(), data.offset());
+            }
+        } catch (Exception e) {
+            LOG.error("Something goes wrong: {}", e.getMessage(), e);
+        }
+    }
+
+
+}
